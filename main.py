274bb4efaa30be511f47696993a6707e6ba33ece
Elyes Saddem <elyes.saddem@etudiant-isi.utm.tn>
2023-04-25 01:03:03 +0200

    added

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from lexer_tn import Lexer
 from parser_tn import Parser
 
-text_input = "code Bagra {}"
+text_input = "code Bagra { x = 3 ;}"
 
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
-for token in tokens:
-    print(token)
-# pg = Parser()
-# pg.parse()
-# parser = pg.get_parser()
-# parser.parse(tokens).eval()
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
