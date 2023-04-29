a5ca3b1c53470abc02a55fa1f2d3b2ec169099b3
Elyes Saddem <elyes.saddem@etudiant-isi.utm.tn>
2023-04-29 20:58:16 +0200

    Fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lexer_tn import Lexer
 from parser_tn import Parser
-text_input = "code Bagra {aadad x1=1;aadad x=-3;jomla y1=\"test\";jomla y2=\"test\";jomla y=y1;aadad z=8;aadad q=-2;aadad i = 0;karrer i men x ila x1 { ken(x1==1){ekteb(\"test\");}}}"
+text_input = "code Bagra {aadad x1=1;aadad x=-3;jomla y1=\"test\";jomla y2=\"test\";jomla y3=\"Hello\";jomla y=y1;aadad z=8;aadad q=-2;aadad i = 0;karrer i men x ila x1 { ken(x1==1){ekteb(y1 + y3);}}}"
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
 # for token in tokens:
--- parser_tn.py
+++ parser_tn.py
@@ -256,12 +256,8 @@ class Parser():
             return None
         @self.pg.production('STRINGS : STRING PLUS STRINGS')
         def program_production(p):
-
             return Token(p[0].value + "+" + p[2].value)
         @self.pg.production('STRINGS : STRING')
-        def program_production(p):
-            return Token(p[0].value)
-        @self.pg.production('STRINGS : IDENTIFIER')
         def program_production(p):
             return Token(p[0].value)
         @self.pg.error
